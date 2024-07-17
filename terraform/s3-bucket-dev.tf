22c645ff209c84621f030a9a720bf8fc6eaf8f36
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-17 04:04:23 -0300

    (feature) added s3 buckets, added deployment strategy

---
+++ terraform/s3-bucket-dev.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "s3_dev" {
+  bucket   = "${var.aws_s3_bucket_name}-dev"
+  provider = aws.aws_provider
+  lifecycle {
+    prevent_destroy = false
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_dev" {
+  depends_on = [aws_s3_bucket.s3_dev]
+  bucket     = aws_s3_bucket.s3_dev.bucket
+  provider   = aws.aws_provider
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_dev" {
+  depends_on          = [aws_s3_bucket.s3_dev]
+  bucket              = aws_s3_bucket.s3_dev.bucket
+  provider            = aws.aws_provider
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_policy" "s3_policy_dev" {
+  depends_on = [aws_s3_bucket_public_access_block.s3_dev]
+  bucket     = aws_s3_bucket.s3_dev.bucket
+  provider   = aws.aws_provider
+  policy     = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "PublicReadGetObject",
+              "Effect": "Allow",
+              "Principal": "*",
+              "Action": "s3:GetObject",
+              "Resource": "arn:aws:s3:::${aws_s3_bucket.s3_dev.bucket}/*"
+          }
+      ]
+    }
+  EOF
+}
